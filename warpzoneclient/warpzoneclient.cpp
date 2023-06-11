3d61fe8e5c7ff122a19b434b0e0f596563be3b89
g3tsyst3m <musicmancorley@gmail.com>
2023-06-10 23:28:57 -0400

    Add files via upload

---
+++ warpzoneclient/warpzoneclient.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <Windows.h>
+
+const int MESSAGE_SIZE = 512;
+
+int main()
+{
+	LPCWSTR pipeName = L"\\\\127.0.0.1\\pipe\\warpzone8";
+	HANDLE clientPipe = NULL;
+	BOOL isPipeRead = true;
+	wchar_t message[MESSAGE_SIZE] = { 0 };
+	DWORD bytesRead = 0;
+
+	std::wcout << "Connecting to " << pipeName << std::endl;
+	clientPipe = CreateFile(pipeName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+
+	while (isPipeRead) {
+		isPipeRead = ReadFile(clientPipe, &message, MESSAGE_SIZE, &bytesRead, NULL);
+		std::wcout << "Received message: " << message;
+	}
+
+	return 0;
+}
